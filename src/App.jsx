f8ae5eb25a9a47d5c31cc06da38a3fe1a11181bf
irvin-ro <irvingmuzic@gmail.com>
2026-03-25 14:00:08 +0200

    hello world 1 sprint

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+
+
+function App() {
+  
+
+  return (
+    <div dir="rtl">
+    <div className="min-h-screen bg-blue-50 py-10 ">
+      <div className="max-w-4xl mx-auto px-6">
+
+        <div className="mb-12">
+          <h1 className="text-5xl font-bold text-blue-900 mb-4">
+           חני-טיק : מערכת חניה חכמה
+          </h1>
+
+        </div>
+
+        <div className="bg-blue-100 rounded-lg shadow-md p-8 mb-8">
+          <h2 className="text-3xl font-bold text-blue-900 mb-4">איך אנחנו עוזרים לכם?</h2> 
+          <p className="text-blue-800 text-lg leading-relaxed">
+בעזרת חני-טיק תוכלו למצוא חנייה קרובה בקלות ובמהירות, עם עדכונים בזמן אמת על זמינות החניות.          </p>
+        </div>
+
+      </div>
+     </div>
+    </div>
+  )
+  
+}
+
+export default App
